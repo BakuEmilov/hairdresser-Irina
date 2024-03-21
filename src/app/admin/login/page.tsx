e885bcc13216d00236fbf5324b5030cd7e4adc0b
BakuEmilov <emilovbaku530@gmail.com>
2024-03-22 01:35:43 +0600

    changed the login section

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -1,71 +1,53 @@
-import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
-  CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import Link from "next/link";
 
 const LoginAdmin = () => {
   return (
-    <div className="flex justify-center items-center h-screen">
-      <Tabs defaultValue="account" className="w-[400px]">
-        <TabsList className="grid w-full grid-cols-2">
-          <TabsTrigger value="account">Account</TabsTrigger>
-          <TabsTrigger value="password">Password</TabsTrigger>
-        </TabsList>
-        <TabsContent value="account">
-          <Card>
-            <CardHeader>
-              <CardTitle>Account</CardTitle>
-              <CardDescription>
-                Make changes to your account here. Click save when youre done.
-              </CardDescription>
-            </CardHeader>
-            <CardContent className="space-y-2">
-              <div className="space-y-1">
-                <Label htmlFor="name">Name</Label>
-                <Input id="name" defaultValue="Pedro Duarte" />
+    <div className="flex items-center justify-center h-screen">
+      <Card className="w-[400px]">
+        <CardHeader className="text-center">
+          <CardTitle>Регистрация</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="name">
+                  Имя или название Вашей парикмахерской
+                </Label>
+                <Input id="name" />
               </div>
-              <div className="space-y-1">
-                <Label htmlFor="username">Username</Label>
-                <Input id="username" defaultValue="@peduarte" />
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="name">Email</Label>
+                <Input id="email" />
               </div>
-            </CardContent>
-            <CardFooter>
-              <Button>Save changes</Button>
-            </CardFooter>
-          </Card>
-        </TabsContent>
-        <TabsContent value="password">
-          <Card>
-            <CardHeader>
-              <CardTitle>Password</CardTitle>
-              <CardDescription>
-                Change your password here. After saving, youll be logged out.
-              </CardDescription>
-            </CardHeader>
-            <CardContent className="space-y-2">
-              <div className="space-y-1">
-                <Label htmlFor="current">Current password</Label>
-                <Input id="current" type="password" />
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="name">Введите новый пароль</Label>
+                <Input id="password" type="password" />
               </div>
-              <div className="space-y-1">
-                <Label htmlFor="new">New password</Label>
-                <Input id="new" type="password" />
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="name">Повторите пароль</Label>
+                <Input id="password" type="password" />
               </div>
-            </CardContent>
-            <CardFooter>
-              <Button>Save password</Button>
-            </CardFooter>
-          </Card>
-        </TabsContent>
-      </Tabs>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="flex justify-between">
+          <Button variant="outline">Отмена</Button>
+          <Link href="/admin/login/hairdresser-details">
+            <Button>Дальше</Button>
+          </Link>
+        </CardFooter>
+      </Card>
     </div>
   );
 };
