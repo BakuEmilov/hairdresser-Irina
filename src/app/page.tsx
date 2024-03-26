a9b57912e74eab16fd5428fa46d0f6cc86183b5b
frontend2006 <aziretsarapov@gmail.com>
2024-03-26 12:02:31 +0600

    I have done workDay

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import AnotherWay from '@/register/anotherWay/page';
 import React from 'react'
 
 const Home = () => {
   return (
     <div>
-      <AnotherWay />
+      Home
     </div>
   )
 }
---
+++ src/app/workDay/page.tsx
@@ -0,0 +1,14 @@
+import WorkingDay from '@/components/WorkingDay/WorkingDay';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import React from 'react'
+
+const WorkDay = () => {
+  return (
+    <div>
+      <WorkingDay />
+    </div>
+  )
+}
+
+export default WorkDay;
