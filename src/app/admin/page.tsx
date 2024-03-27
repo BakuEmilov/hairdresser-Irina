22c478f7662214e3c06eeb58c054d7d57a226d88
BakuEmilov <emilovbaku530@gmail.com>
2024-03-27 17:14:58 +0600

    I made a table of clients

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import AdminLayout from "./components/layout/AdminLayout";
+import Customers from "./components/customers/Customers";
 
 const AdminPage = () => {
   return (
     <AdminLayout>
+      <Customers/>
     </AdminLayout>
   );
 };
