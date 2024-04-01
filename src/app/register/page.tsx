8ed33c2dc3bb60cfc00f7e130fff223a43e43073
BakuEmilov <emilovbaku530@gmail.com>
2024-04-02 02:10:11 +0600

    divided the register into roles

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,38 +1,37 @@
-"use client"
+"use client";
 
-import React from 'react';
-import { Button } from '@/components/ui/button';
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
-import { Input } from '@/components/ui/input';
-import Link from 'next/link';
+import React from "react";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import Link from "next/link";
 
 const Register = () => {
   return (
-    <div className='flex items-center justify-center h-screen'>
-      <Card className='w-[400px]'>
+    <div className="flex items-center justify-center h-screen">
+      <Card className="w-[400px]">
         <CardHeader className="text-center">
-          <CardTitle>Пароль</CardTitle>
+          <CardTitle>Выбор роли</CardTitle>
         </CardHeader>
         <CardContent>
-          <form>
-            <div className="grid w-full items-center gap-4">
-              <div className="flex flex-col space-y-1.5">
-                <Input placeholder='Пароль' id='password'/>
-              </div>
-            </div>
-          </form>
-        </CardContent>
-        <CardFooter>
-          <div className='grid place-content-center w-full gap-5'>
-            <Button>Пройти к записи</Button>
-            <Link href={"/register/anotherWay"}>
-              <Button>Пройти к записи другим способом</Button>
+          <div className="flex items-center justify-center gap-x-8">
+            <Link href={"/"}>
+              <Button>Я Клиент</Button>
+            </Link>
+            <Link href={"/admin/login"}>
+              <Button>Я Парикмахер</Button>
             </Link>
           </div>
-        </CardFooter>
+        </CardContent>
       </Card>
     </div>
-  )
-}
+  );
+};
 
 export default Register;
