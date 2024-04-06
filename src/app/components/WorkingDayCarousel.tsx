e03d8e53bbc49bad88a2557d8bb8d55158e1b8a4
Esen <s7b5en@gmail.com>
2024-04-07 02:22:54 +0500

    redesign timtable a bit

--- src/app/components/WorkingDayCarousel.tsx
+++ src/app/components/WorkingDayCarousel.tsx
@@ -7,95 +7,88 @@ import {
 } from "@/components/ui/carousel";
 
 import WorkingDay from "@/app/components/WorkingDay";
+import * as fns from "date-fns";
+
+export type Period = {
+  start: Date;
+  end: Date;
+  occupied?: boolean;
+};
+
+function generateTimetable(
+  startTime: Date,
+  endTime: Date,
+  intervalMinutes: number
+): Period[] {
+  const timetable: Period[] = [];
+
+  let currentDate = startTime;
+
+  while (currentDate <= endTime) {
+    timetable.push({
+      start: currentDate,
+      end: fns.addMinutes(currentDate, intervalMinutes),
+      occupied: Boolean(Math.random() > 0.5),
+    });
+
+    currentDate = fns.addMinutes(currentDate, intervalMinutes);
+  }
+
+  return timetable;
+}
+
+const generatePeriodsFor = ({
+  workingHours = 8,
+  days = 14,
+  interval = 60,
+}: {
+  workingHours: number;
+  days: number;
+  interval: number;
+}) => {
+  const start = new Date();
+  start.setHours(9);
+  start.setMinutes(0);
+  start.setSeconds(0);
+
+  const startingtimes = Array.from({ length: days }, (_, i) => {
+    return fns.addDays(start, i);
+  });
+
+  console.log("asdasd", start, fns.addHours(start, workingHours));
+
+  const fullDay = startingtimes.map((start) => {
+    return generateTimetable(
+      start,
+      fns.addHours(start, workingHours),
+      interval
+    );
+  });
+
+  return fullDay;
+};
+
+const twoWeeks = generatePeriodsFor({
+  days: 14,
+  workingHours: 8,
+  interval: 60,
+});
 
 const WorkingDayCarousel = () => {
   return (
-    <div className="mt-20">
-      <Carousel className="w-full max-w-[90%] mx-auto">
-        <CarouselContent className="flex items-center">
-          <CarouselItem className="lg:basis-1/3">
-            <WorkingDay
-              key="1"
-              day={[
-                { id: 11, time: "9:00 - 10:00", link: "Записаться" },
-                { id: 12, time: "10:00 - 11:00", link: "Записаться" },
-                { id: 13, time: "11:00 - 12:00", link: "Занято" },
-                { id: 14, time: "13:00 - 14:00", link: "Записаться" },
-                { id: 15, time: "15:00 - 16:00", link: "Записаться" },
-              ]}
-              date="28 марта"
-            />
-          </CarouselItem>
-          <CarouselItem className="lg:basis-1/3">
-            <WorkingDay
-              key="1"
-              day={[
-                { id: 11, time: "9:00 - 10:00", link: "Записаться" },
-                { id: 12, time: "10:00 - 11:00", link: "Записаться" },
-                { id: 13, time: "11:00 - 12:00", link: "Занято" },
-                { id: 14, time: "13:00 - 14:00", link: "Записаться" },
-                { id: 15, time: "15:00 - 16:00", link: "Записаться" },
-              ]}
-              date="28 марта"
-            />
-          </CarouselItem>
-          <CarouselItem className="lg:basis-1/3">
-            <WorkingDay
-              key="1"
-              day={[
-                { id: 11, time: "9:00 - 10:00", link: "Записаться" },
-                { id: 12, time: "10:00 - 11:00", link: "Записаться" },
-                { id: 13, time: "11:00 - 12:00", link: "Занято" },
-                { id: 14, time: "13:00 - 14:00", link: "Записаться" },
-                { id: 15, time: "15:00 - 16:00", link: "Записаться" },
-              ]}
-              date="28 марта"
-            />
-          </CarouselItem>
-          <CarouselItem className="lg:basis-1/3">
-            <WorkingDay
-              key="1"
-              day={[
-                { id: 11, time: "9:00 - 10:00", link: "Записаться" },
-                { id: 12, time: "10:00 - 11:00", link: "Записаться" },
-                { id: 13, time: "11:00 - 12:00", link: "Занято" },
-                { id: 14, time: "13:00 - 14:00", link: "Записаться" },
-                { id: 15, time: "15:00 - 16:00", link: "Записаться" },
-              ]}
-              date="28 марта"
-            />
-          </CarouselItem>
-          <CarouselItem className="lg:basis-1/3">
-            <WorkingDay
-              key="1"
-              day={[
-                { id: 11, time: "9:00 - 10:00", link: "Записаться" },
-                { id: 12, time: "10:00 - 11:00", link: "Записаться" },
-                { id: 13, time: "11:00 - 12:00", link: "Занято" },
-                { id: 14, time: "13:00 - 14:00", link: "Записаться" },
-                { id: 15, time: "15:00 - 16:00", link: "Записаться" },
-              ]}
-              date="28 марта"
-            />
-          </CarouselItem>
-          <CarouselItem className="lg:basis-1/3">
-            <WorkingDay
-              key="1"
-              day={[
-                { id: 11, time: "9:00 - 10:00", link: "Записаться" },
-                { id: 12, time: "10:00 - 11:00", link: "Записаться" },
-                { id: 13, time: "11:00 - 12:00", link: "Занято" },
-                { id: 14, time: "13:00 - 14:00", link: "Записаться" },
-                { id: 15, time: "15:00 - 16:00", link: "Записаться" },
-              ]}
-              date="28 марта"
-            />
-          </CarouselItem>
-        </CarouselContent>
-        <CarouselPrevious className="absolute left-0" />
-        <CarouselNext className="absolute right-0" />
-      </Carousel>
-    </div>
+    <Carousel className="contents mx-auto w-max">
+      <CarouselContent className="flex items-center">
+        {twoWeeks.map((day, index) => {
+          return (
+            <CarouselItem className="basis-[400px] ">
+              <WorkingDay key={index} timetable={day} />
+            </CarouselItem>
+          );
+        })}
+      </CarouselContent>
+      <CarouselPrevious className="absolute left-0" />
+      <CarouselNext className="absolute right-0" />
+    </Carousel>
   );
 };
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <main>{children}</main>
+        <main className="container">{children}</main>
       </body>
     </html>
   );
